2cffb2c51b6e19081110711583cdbc74967918d1
a_lampcat <fatkulin@a2design.biz>
2014-04-18 16:37:13 +0700

    alpha-hipster+setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hipsterlib',
+    url='https://github.com/a2design-company/hipster',
+    version='0.1',
+    packages=['hipster'],
+    author='Andrew Fatkulin',
+    author_email='a.lampcat@gmail.com',
+    license='MIT',
+    long_description=open('README.md').read()
+)
